ca01f3deea5bc9352fdeed2fe230424d84bcf913
tarikhuber <huber.tarik@gmail.com>
2018-02-04 19:36:34 +0100

    Update firebase configs

--- src/config.js
+++ src/config.js
@@ -6,28 +6,24 @@ import grants from './grants';
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650',
+    apiKey: "AIzaSyA8qQ2p_1UXRJnR3zDZRa7VrSlKVsJUop8",
+    authDomain: "ugst-proplan.firebaseapp.com",
+    databaseURL: "https://ugst-proplan.firebaseio.com",
+    projectId: "ugst-proplan",
+    storageBucket: "ugst-proplan.appspot.com",
+    messagingSenderId: "604966397909"
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824',
+    apiKey: "AIzaSyDuJXk0p_44oXmQIa43gObQoMjZWUTgKjM",
+    authDomain: "dev-ugst-proplan.firebaseapp.com",
+    databaseURL: "https://dev-ugst-proplan.firebaseio.com",
+    projectId: "dev-ugst-proplan",
+    storageBucket: "dev-ugst-proplan.appspot.com",
+    messagingSenderId: "113490407647"
   },
   firebase_providers: [
     'google.com',
-    'facebook.com',
-    'twitter.com',
-    'github.com',
-    'password',
-    'phone',
+    'password'
   ],
   initial_state: {
     theme: 'dark',

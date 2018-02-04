237ee85d14658695b5e7815e335b7a7f5c41c276
tarikhuber <huber.tarik@gmail.com>
2018-02-04 20:49:53 +0100

    Fixed correction in users count

--- functions/db/usersCount/onDelete.f.js
+++ functions/db/usersCount/onDelete.f.js
@@ -1,11 +1,11 @@
-const functions = require('firebase-functions');
-const counting = require('../../utils/counting');
+const functions = require('firebase-functions')
+const counting = require('../../utils/counting')
 
 exports = module.exports = functions.database
   .ref('/users_count')
   .onDelete(event => {
     return Promise.all([
-      counting.handleRecount(event, 'users', 8),
-      counting.handleProviderRecount(event),
-    ]);
-  });
+      counting.handleRecount(event, 'users', 0),
+      counting.handleProviderRecount(event)
+    ])
+  })
